f116ca25ec916676f881818577683875429982cc
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-03-31 20:29:37 -0300

    Update App.js

--- my-app/App.js
+++ my-app/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, FlatList, ScrollView, SafeAreaView, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, FlatList } from "react-native";
+import { Card } from "react-native-elements";
 
 
 const sorvetesCremosos = [
@@ -18,13 +19,16 @@ const sorvetesRefrescantes = [
   { id: "5", name: "Coco" },
 ];
 
-const sectionListData = [
+const sectionListData1 = [
   {
-    title: "Sorvetes Cremosos",
+    title: "SORVETES CREMOSOS",
     data: ["Chcolate Belga", "Pisache", "Cookies & Cream", "Baunilha de Madagascar", "Napolitano"],
   },
+];
+
+const sectionListData2 = [
   {
-    title: "Sorvetes Refrescantes",
+    title: "SORVETES REFRESCANTES",
     data: ["Manga", "Limão Siciliano", "Maracujá", "Frutas Vermelhas", "Coco"],
   },
 ];
@@ -33,16 +37,30 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollContainer}>
-        <Text style={styles.title01}> Welcome to Gelatto & Cia</Text>
+        <Text style={styles.title01}> WELCOME TO GELATTO & CIA</Text>
         <Text style={styles.subtitle}>The best place to buy your ice cream</Text>
-        <Text style={styles.title02}>Menu</Text>
+        <Text style={styles.title02}>MENU</Text>
+        <div style={styles.cards }>
+        <div style={styles.card1}>
         <FlatList
-          data={sectionListData}
+          data={sectionListData1}
           renderItem={({ item }) => (
             <Text style={styles.sectionTitle}>{item.title}</Text>
           )}
           keyExtractor={(item) => item.title}
         />
+        </div>
+        <div style={styles.card2}>
+        <FlatList
+          data={sectionListData2}
+          renderItem={({ item }) => (
+            <Text style={styles.sectionTitle}>{item.title}</Text>
+          )}
+          keyExtractor={(item) => item.title}
+        />
+        </div>
+        </div>
+        <Text style={styles.title02}>SORVETES</Text>
         <FlatList
           data={sorvetesCremosos}
           renderItem={({ item }) => (
@@ -50,6 +68,7 @@ export default function App() {
           )}
           keyExtractor={(item) => item.id}
         />
+        
         <FlatList
           data={sorvetesRefrescantes}
           renderItem={({ item }) => (
@@ -61,3 +80,64 @@ export default function App() {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    fontFamily: "Arial",
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+  scrollContainer: {
+    padding: 30,
+  },
+  title01: {
+    fontSize: 45,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#c15b78",
+  },
+  subtitle: {
+    fontSize: 25,
+    padding: 10,
+    textAlign: "center",
+    color: "#f6c8cc",
+  },
+  title02: {
+    padding:60,
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#c15b78",
+  },
+  cards: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+  },
+  card1: {
+    width: "25%",
+    height: 60,
+    backgroundColor: "#f6c8cc",
+    borderRadius: 10,
+  },
+  card2: {
+    width: "25%",
+    height: 60,
+    backgroundColor: "#f6c8cc",
+    borderRadius: 10,
+  },
+  sectionTitle: {
+    fontSize: 25,
+    padding: 10,
+    color: "#000000",
+    textAlign: "center",
+  },
+  item: {
+    fontSize: 25,
+    padding: 10,
+    textAlign: "center",
+    justifyContent: "space-between",
+    color: "#000000",
+  },
+});
